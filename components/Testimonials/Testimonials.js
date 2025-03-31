44f4e4284fb2c815c4854c4336af376e334dac26
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-03-31 16:28:52 -0700

    Error fix

--- components/Testimonials/Testimonials.js
+++ components/Testimonials/Testimonials.js
@@ -2,17 +2,16 @@ import { gql } from '@apollo/client';
 import {
   FaChevronCircleLeft,
   FaChevronCircleRight,
-  FaQuoteRight,
-} from 'react-icons/fa';
+} from 'react-icons/fa'; // ✅ FaQuoteRight removed
 import className from 'classnames/bind';
 import { Carousel } from 'react-responsive-carousel';
 
-import TestimonialItem from '../TestimonialItem';
 import Slide1 from './Slide1';
 import Slide2 from './Slide2';
 import Slide3 from './Slide3';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Testimonials.module.scss';
+
 const cx = className.bind(styles);
 
 /**
@@ -24,33 +23,30 @@ const cx = className.bind(styles);
  */
 export default function Testimonials({ testimonials }) {
   return (
-    <>
-      <div className={cx('container')}>
-
-        <Carousel
-          showIndicators={false}
-          showThumbs={false}
-          renderArrowPrev={(clickHandler) => (
-            <FaChevronCircleLeft
-              className={cx('arrow')}
-              onClick={clickHandler}
-            />
-          )}
-          renderArrowNext={(clickHandler) => (
-            <FaChevronCircleRight
-              className={cx('arrow')}
-              onClick={clickHandler}
-            />
-          )}
-          infiniteLoop={true}
-          showStatus={false}
-        >
-            <Slide1 />
-            <Slide2 />
-            <Slide3 />
-        </Carousel>
-      </div>
-    </>
+    <div className={cx('container')}>
+      <Carousel
+        showIndicators={false}
+        showThumbs={false}
+        renderArrowPrev={(clickHandler) => (
+          <FaChevronCircleLeft
+            className={cx('arrow')}
+            onClick={clickHandler}
+          />
+        )}
+        renderArrowNext={(clickHandler) => (
+          <FaChevronCircleRight
+            className={cx('arrow')}
+            onClick={clickHandler}
+          />
+        )}
+        infiniteLoop
+        showStatus={false}
+      >
+        <Slide1 />
+        <Slide2 />
+        <Slide3 />
+      </Carousel>
+    </div>
   );
 }
 
