c37d6830c8a9cf8c737da58e439f6e409373bcca
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-04-02 19:09:07 -0700

    Hero work

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import Image from 'next/image';
+import Button from 'components/Button';
 
 import styles from './Hero.module.scss';
 
@@ -12,14 +13,18 @@ const cx = classNames.bind(styles);
 export default function Hero() {
   return (
     <div className={cx('hero-wrap')}>
-        <Image
-            src="/static/hero.jpg"
-            width="100"
-            height="100"
-            objectFit="cover"
-            layout="responsive"
-        />
       <div className="container">
+        <div className={cx('hero-content')}>
+            <Image
+                    src="/static/welcome-to-bm.png"
+                    width="100"
+                    height="100"
+                    objectFit="contain"
+                    layout="responsive"
+                />
+                <p>An exclusive residential community designed by <span>Cal Poly Partners</span> for university and faculty staff.</p>
+                <Button href="/available-homes/">Available Homes</Button>
+        </div>
       </div>
     </div>
   );
